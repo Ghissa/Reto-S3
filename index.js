7dd49ac7be52a3305639a3120a809c88c30b921d
Ghissa <clarissa.palomino@pucp.edu.pe>
2022-02-19 22:40:36 -0500

    Finalización de la versión 1.2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+//document.addEventListener('DOMContentLoaded'); () => {
+//const elementosCarousel= document.querySelectorAll('.carousel');
+//M.Carousel.init(elementosCarousel),{
+//duration: 150 
+ //
